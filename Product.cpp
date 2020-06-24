a8fd61b7313af97b7d391d5d1435d956174eea54
Adwait-Shirodkar <67391915+Adwait-Shirodkar@users.noreply.github.com>
2020-06-25 01:31:24 +0530

    Practise codes for 1st MYCAPTAIN submission
    I am late to upload this but CAPTAIN, please check it!! THANK YOU!

---
+++ Product.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a;
+	float b;
+	float c;
+	cout<<"Enter two numbers: ";
+	cin>>a;
+	cout<<endl;
+	cin>>b;
+	c=a*b;
+	cout<<"The product= "<<c;
+	return 0;
+}
